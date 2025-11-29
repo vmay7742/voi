72b108c676b25b04e52e48defb2395fb14afd4d9
vmay7742 <154700235+vmay7742@users.noreply.github.com>
2025-11-29 17:54:40 +0000

    Delete appimages.sh

--- appimages.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p "/home/v/Apps" && cd "/home/v/Apps" && wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.10.3/Obsidian-1.10.3.AppImage && chmod +x Obsidian-1.10.3.AppImage && sudo ln -s /home/v/Apps/Obsidian-1.10.3.AppImage /usr/bin/obsidian && wget https://github.com/localsend/localsend/releases/download/v1.17.0/LocalSend-1.17.0-linux-x86-64.AppImage && chmod +x LocalSend-1.17.0-linux-x86-64.AppImage && sudo ln -s /home/v/Apps/LocalSend-1.17.0-linux-x86-64.AppImage /usr/bin/localsend && wget https://download.blender.org/release/Blender5.0/blender-5.0.0-linux-x64.tar.xz && sudo ln -s /home/v/Apps/blender-5.0.0-linux-x64/blender /usr/bin/blender
-
-cd
